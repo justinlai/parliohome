1f642fa16109fa173d580ac7ca25e35e6553f63f
justinlai <justin@parlio.com>
2015-07-10 16:23:39 -0700

    initialized scroll + navbar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,11 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+ $('#fullpage').fullpage({
+        //Navigation
+        // menu: true,
+        lockAnchors: false,
+        anchors:['Parlio', 'Perspectives','Community','Parlio Is For You'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Parlio', 'Perspectives','Community','Parlio Is For You'],
+        // showActiveTooltip: false,
+    });});
