d1b7f2e381b61e933d223b8a5d486b9d2e3b42b2
justinlai <justin@parlio.com>
2015-07-15 16:24:24 -0700

    failed bounce :(

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,32 @@ $(document).ready(function() {
         lockAnchors: false,
         anchors:['Parlio', 'Perspectives','Community','Parlio Is For You'],
         navigation: true,
-        navigationPosition: 'right',
+        navigationPosition: 'left',
+        verticalCentered: true,
         navigationTooltips: ['Parlio', 'Perspectives','Community','Parlio Is For You'],
-        // showActiveTooltip: false,
-    });});
+    });
+ 	var bounce = new Bounce();
+	bounce
+	  .translate({
+	    from: { x: -300, y: 0 },
+	    to: { x: 0, y: 0 },
+	    duration: 600,
+	    stiffness: 4
+	  })
+	  .scale({
+	    from: { x: 1, y: 1 },
+	    to: { x: 0.1, y: 2.3 },
+	    easing: "sway",
+	    duration: 800,
+	    delay: 65,
+	    stiffness: 2
+	  })
+	  .scale({
+	    from: { x: 1, y: 1},
+	    to: { x: 5, y: 1 },
+	    easing: "sway",
+	    duration: 300,
+	    delay: 30,
+	  })
+	  .applyTo(document.querySelectorAll(".circle-one"));
+});
