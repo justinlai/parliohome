5fc9223c12ce3e34045744066fe9c796c2dab213
justinlai <justin@parlio.com>
2015-07-16 15:20:35 -0700

    Wow.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,30 +7,7 @@ $(document).ready(function() {
         navigation: true,
         navigationPosition: 'left',
         verticalCentered: true,
+        scrollBar:true,
         navigationTooltips: ['Parlio', 'Perspectives','Community','Parlio Is For You'],
     });
- 	var bounce1 = new Bounce();
-	bounce1.translate({
-    from: { x: 0, y: 300 },
-    to: { x: 0, y: 1000 },
-    duration: 10000,
-    delay: 1000,
-    stiffness: 4
-  	})
-  // .scale({
-  //   from: { x: 1, y: 1 },
-  //   to: { x: 0.1, y: 2.3 },
-  //   easing: "sway",
-  //   duration: 800,
-  //   delay: 65,
-  //   stiffness: 2
-  // })
-  // .scale({
-  //   from: { x: 1, y: 1},
-  //   to: { x: 5, y: 1 },
-  //   easing: "sway",
-  //   duration: 300,
-  //   delay: 30,
-  // })
-  bounce1.applyTo(document.querySelectorAll(".circle-one"));
 });
