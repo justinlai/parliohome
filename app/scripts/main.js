90e60211eb5e28c01f2b50ec50cc4ebc1b1a1522
justinlai <justin@parlio.com>
2015-07-16 11:54:02 -0700

    fixed bounce

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,31 +9,28 @@ $(document).ready(function() {
         verticalCentered: true,
         navigationTooltips: ['Parlio', 'Perspectives','Community','Parlio Is For You'],
     });
- 	var bounce = new Bounce();
-	bounce
-  		.translate({
-    from: { x: -300, y: 0 },
-    to: { x: 0, y: 0 },
-    duration: 600,
+ 	var bounce1 = new Bounce();
+	bounce1.translate({
+    from: { x: 0, y: 300 },
+    to: { x: 0, y: 1000 },
+    duration: 10000,
+    delay: 1000,
     stiffness: 4
   	})
-  .scale({
-    from: { x: 1, y: 1 },
-    to: { x: 0.1, y: 2.3 },
-    easing: "sway",
-    duration: 800,
-    delay: 65,
-    stiffness: 2
-  })
-  .scale({
-    from: { x: 1, y: 1},
-    to: { x: 5, y: 1 },
-    easing: "sway",
-    duration: 300,
-    delay: 30,
-  })
-
-	bounce.applyTo($(".circle-one")).then(function() { 
-  console.log("Animation Complete"); 
-});
+  // .scale({
+  //   from: { x: 1, y: 1 },
+  //   to: { x: 0.1, y: 2.3 },
+  //   easing: "sway",
+  //   duration: 800,
+  //   delay: 65,
+  //   stiffness: 2
+  // })
+  // .scale({
+  //   from: { x: 1, y: 1},
+  //   to: { x: 5, y: 1 },
+  //   easing: "sway",
+  //   duration: 300,
+  //   delay: 30,
+  // })
+  bounce1.applyTo(document.querySelectorAll(".circle-one"));
 });
