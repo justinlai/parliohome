18a7a72c5ea88cedc331d4f0d4bdbb55e0b42aa8
justinlai <justin@parlio.com>
2015-07-15 18:12:26 -0700

    bounce.js added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,26 +11,29 @@ $(document).ready(function() {
     });
  	var bounce = new Bounce();
 	bounce
-	  .translate({
-	    from: { x: -300, y: 0 },
-	    to: { x: 0, y: 0 },
-	    duration: 600,
-	    stiffness: 4
-	  })
-	  .scale({
-	    from: { x: 1, y: 1 },
-	    to: { x: 0.1, y: 2.3 },
-	    easing: "sway",
-	    duration: 800,
-	    delay: 65,
-	    stiffness: 2
-	  })
-	  .scale({
-	    from: { x: 1, y: 1},
-	    to: { x: 5, y: 1 },
-	    easing: "sway",
-	    duration: 300,
-	    delay: 30,
-	  })
-	  .applyTo(document.querySelectorAll(".circle-one"));
+  		.translate({
+    from: { x: -300, y: 0 },
+    to: { x: 0, y: 0 },
+    duration: 600,
+    stiffness: 4
+  	})
+  .scale({
+    from: { x: 1, y: 1 },
+    to: { x: 0.1, y: 2.3 },
+    easing: "sway",
+    duration: 800,
+    delay: 65,
+    stiffness: 2
+  })
+  .scale({
+    from: { x: 1, y: 1},
+    to: { x: 5, y: 1 },
+    easing: "sway",
+    duration: 300,
+    delay: 30,
+  })
+
+	bounce.applyTo($(".circle-one")).then(function() { 
+  console.log("Animation Complete"); 
+});
 });
